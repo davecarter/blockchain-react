3e0f8df92346e043594d21bb93ff551b5c568570
David Garcia <david.garcia@Davids-MacBook-Pro.local>
2020-02-11 00:02:21 +0100

    feat(domain): btcCurrencyEntityFactory mapper dependency injection

--- src/domain/btc/mapper/factory.js
+++ src/domain/btc/mapper/factory.js
@@ -1,6 +1,11 @@
 import {FromApiResponseToBtcEntityMapper} from './FromApiResponseToBtcEntityMapper'
+import {BtcCurrencyEntityFactory} from '../entities/factory'
+import {config} from '../../config'
 
 export class BtcMapperFactory {
   static fromApiResponseToBtcEntityMapper = () =>
-    new FromApiResponseToBtcEntityMapper()
+    new FromApiResponseToBtcEntityMapper({
+      config,
+      btcCurrencyEntityFactory: BtcCurrencyEntityFactory.btcCurrencyEntity
+    })
 }
