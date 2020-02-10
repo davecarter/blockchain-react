a427033c873193cdf9fc72c04b3622636d5ca47f
David Garcia <david.garcia@adevinta.com>
2020-02-11 00:06:22 +0100

    refactor(domain): adds btcCurrencyEntityFactory to mapper

--- src/domain/btc/mapper/FromApiResponseToBtcEntityMapper.js
+++ src/domain/btc/mapper/FromApiResponseToBtcEntityMapper.js
@@ -1,9 +1,28 @@
 export class FromApiResponseToBtcEntityMapper {
+  constructor({config, btcCurrencyEntityFactory}) {
+    this._config = config
+    this._btcCurrencyEntityFactory = btcCurrencyEntityFactory
+  }
+
+  // eslint-disable-next-line camelcase
+  formatCurrency = ({rate_float, code}, countryCode) => {
+    const {LOCALE} = this._config
+    return new Intl.NumberFormat(LOCALE[countryCode], {
+      style: 'currency',
+      currency: code,
+      minimumFractionDigits: 0,
+      useGrouping: true
+    }).format(parseInt(rate_float))
+  }
+
   async map(apiResponse) {
-    return {
-      time: apiResponse.time.updated,
-      disclaimer: apiResponse.disclaimer,
-      eur: apiResponse.bpi?.EUR?.rate
-    }
+    const {EUR, USD} = apiResponse?.bpi
+
+    return this._btcCurrencyEntityFactory({
+      time: apiResponse?.time?.updated,
+      disclaimer: apiResponse?.disclaimer,
+      eur: await this.formatCurrency(EUR, 'es'),
+      usd: await this.formatCurrency(USD, 'us')
+    })
   }
 }
