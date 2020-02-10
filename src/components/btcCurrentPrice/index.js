8331d55fd5ead9fbad63dcea321547d6cf5e85ed
David Garcia <david.garcia@adevinta.com>
2020-02-11 00:04:40 +0100

    refactor(components): adds usd to btcCurrentPrice component

--- src/components/btcCurrentPrice/index.js
+++ src/components/btcCurrentPrice/index.js
@@ -4,24 +4,26 @@ import {domain} from '../../domain'
 export const BtcCurrentPrice = () => {
   const baseClass = 'btcCurrentPrice'
   const [eur, setEur] = useState('')
+  const [usd, setUsd] = useState('')
   useEffect(() => {
     domain
       .get('get_btc_current_price_use_case')
       .execute({currency: 'EUR'})
-      .then(response => {
-        const {eur} = response
+      .then(BtcCurrentPriceResult => {
+        const {eur, usd} = BtcCurrentPriceResult
         setEur(eur)
+        setUsd(usd)
       })
-      .catch(error => window.console.error('error', error))
+      .catch(BtcCurrentPriceError =>
+        window.console.error('error', BtcCurrentPriceError)
+      )
   }, [eur])
-
-  const formatCurrency = currency =>
-    `${currency.replace(',', '.').slice(0, 5)} €`
 
   return (
     <div className={baseClass}>
       <h2 className={`${baseClass}-heading`}>Bitcoin current Price</h2>
-      <h3 className={`${baseClass}-price`}>{formatCurrency(eur)}</h3>
+      <h3 className={`${baseClass}-price`}>{eur}</h3>
+      <h3 className={`${baseClass}-price`}>{usd}</h3>
     </div>
   )
 }
