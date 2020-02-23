d195d3486bd255e188acdd7bcafa8fa0af08ce12
David Garcia <david.garcia@adevinta.com>
2020-02-23 11:48:52 +0100

    chore(Root): replace sui-bundler by webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+
+const DIST_DIR = path.resolve(__dirname, '/dist')
+
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: DIST_DIR,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: [MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(html)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'html-loader',
+          options: {minimize: true}
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+      hash: true
+    }),
+    new MiniCSSExtractPlugin({
+      filename: './css/styles.css'
+    })
+  ]
+}
