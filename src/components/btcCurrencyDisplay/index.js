7850a56a218759e585e1c6cf7e8b1b32a665460b
David Garcia <david.garcia@adevinta.com>
2020-03-22 00:58:53 +0100

    feat(components): adds a heading to Display component and some styling

--- src/components/btcCurrencyDisplay/index.js
+++ src/components/btcCurrencyDisplay/index.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const BtcCurrencyDisplay = ({currentPriceValue, fiatCurrencyCode, updated}) => {
+const BtcCurrencyDisplay = ({currentPriceValue, updated}) => {
   const baseClass = 'btcCurrencyDisplay'
 
   return (
-    <>
-      <span className={`${baseClass}-heading`}>Bitcoin current Price:</span>
+    <div className={baseClass}>
+      <h3 className={`${baseClass}-heading`}>
+        Bitcoin <span className={`${baseClass}-code`}>BTC</span>
+      </h3>
       <span className={`${baseClass}-valueContainer`}>
         <span className={`${baseClass}-priceEUR`}>{currentPriceValue}</span>
+        <span className={`${baseClass}-updated`}>{updated}</span>
       </span>
-      <p>{fiatCurrencyCode}</p>
-      <span>{updated}</span>
-    </>
+    </div>
   )
 }
 
 BtcCurrencyDisplay.propTypes = {
   currentPriceValue: PropTypes.string,
-  fiatCurrencyCode: PropTypes.string,
   updated: PropTypes.string
 }
 
