e3c12c1ee3cc757236da86a453c0577eb606fa47
David Garcia <david.garcia@adevinta.com>
2020-03-22 01:00:09 +0100

    feat(styles): adds some styling to home component heading

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 import {BtcCurrentPrice} from '../btcCurrentPrice'
 
-const HomePage = () => <BtcCurrentPrice />
+const HomePage = () => (
+  <>
+    <header className="homePage-Header">
+      <h1 className="homePage-HeaderTitle">Blockchain based project</h1>
+      <a
+        className="homePage-HeaderAuthor"
+        title="developed by David G"
+        href="https://twitter.com/@d4vecarter"
+      >
+        @d4vecarter
+      </a>
+    </header>
+    <BtcCurrentPrice />
+  </>
+)
 
 export {HomePage}
