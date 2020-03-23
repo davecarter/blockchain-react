8186b9566ac7ccf1e76e04d5556a49d4bef29aaf
David Garcia <david.garcia@adevinta.com>
2020-03-23 23:40:28 +0100

    feat(Root): adds MOCKED_BLOCKCHAIN_DATA object to config file

--- src/domain/config/index.js
+++ src/domain/config/index.js
@@ -5,9 +5,22 @@ const LOCALE = {
   ARS: 'es-AR'
 }
 
+const MOCKED_BLOCKCHAIN_DATA = [
+  {
+    blockId: 0,
+    creationDate: Date.now(),
+    previousHash: 'Genesis',
+    blockData: 'Genesis Block',
+    nonce: 1,
+    currentHash: 0,
+    currentDifficulty: '00'
+  }
+]
+
 const config = {
   API_URL_COINDESK: 'https://api.coindesk.com/v1/bpi/currentprice/',
-  LOCALE
+  LOCALE,
+  MOCKED_BLOCKCHAIN_DATA
 }
 
 export {config}
