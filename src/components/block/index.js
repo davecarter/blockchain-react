6d89fe47c8e63fc8ab0af9b07d67db232c0f41fa
David Garcia <david.garcia@adevinta.com>
2020-03-23 23:39:48 +0100

    feat(components): adds table layout improvements to Block component

--- src/components/block/index.js
+++ src/components/block/index.js
@@ -2,52 +2,60 @@ import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import {domain} from '../../domain/index'
 
-const Block = ({blockNumber}) => {
+const Block = ({blockNumber, previousHash}) => {
   const baseClass = 'block'
   const [blockId, setBlockId] = useState(0)
   const [creationDate, setCreationDate] = useState(0)
-  const [previousHash, setPreviousHash] = useState(0)
   const [blockData, setBlockData] = useState('')
   const [currentHash, setCurrentHash] = useState(0)
 
   useEffect(() => {
-    const {blockId, creationDate, blockData} = domain
+    const {blockId, creationDate, blockData, currentHash} = domain
       .get('get_last_blockchain_block')
-      .execute({blockNumber})
+      .execute({blockNumber, previousHash})
     setBlockId(blockId)
     setCreationDate(creationDate)
-    setPreviousHash(previousHash)
     setBlockData(blockData)
-  }, [blockNumber, blockData, previousHash])
+    setCurrentHash(currentHash)
+  }, [blockNumber, previousHash])
+
+  const handleBlockData = evt => setBlockData(evt.target.value)
 
   return (
     <div className={baseClass}>
-      <h2 className={`${baseClass}-heading`}>BLOCK #{blockId}</h2>
       <table>
-        <tr>
-          <td className={`${baseClass}-labelColumn`}>Creation date:</td>
-          <td className={`${baseClass}-labelData`}>{creationDate}</td>
-        </tr>
-        <tr>
-          <td className={`${baseClass}-labelColumn`}>Previous hash:</td>
-          <td className={`${baseClass}-labelData`}>{previousHash}</td>
-        </tr>
-        <tr>
-          <td className={`${baseClass}-labelColumn`}>
-            <label htmlFor="blockData">Block data:</label>
-          </td>
-          <td>
-            <textarea
-              id="blockData"
-              className={`${baseClass}-data`}
-              value={blockData}
-            />
-          </td>
-        </tr>
-        <tr>
-          <td className={`${baseClass}-labelColumn`}>Current hash:</td>
-          <td className={`${baseClass}-labelData`}>{currentHash}</td>
-        </tr>
+        <thead>
+          <tr>
+            <th className={`${baseClass}-heading`}>BLOCK #{blockId}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className={`${baseClass}-labelColumn`}>Creation date:</td>
+            <td className={`${baseClass}-labelData`}>{creationDate}</td>
+          </tr>
+          <tr>
+            <td className={`${baseClass}-labelColumn`}>Previous hash:</td>
+            <td className={`${baseClass}-labelData`}>{previousHash}</td>
+          </tr>
+          <tr>
+            <td className={`${baseClass}-labelColumn`}>
+              <label htmlFor="blockData">Block data:</label>
+            </td>
+            <td>
+              <textarea
+                id="blockData"
+                className={`${baseClass}-data`}
+                value={blockData}
+                onChange={handleBlockData}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td className={`${baseClass}-labelColumn`}>Current hash:</td>
+            <td className={`${baseClass}-labelData`}>{currentHash}</td>
+          </tr>
+        </tbody>
       </table>
 
       <button className={`${baseClass}-button`}>Mine Block!</button>
@@ -56,9 +64,13 @@ const Block = ({blockNumber}) => {
 }
 
 Block.propTypes = {
-  blockNumber: PropTypes.number
+  blockNumber: PropTypes.number,
+  previousHash: PropTypes.string
 }
 
-Block.defaultProps = {blockNumber: 0}
+Block.defaultProps = {
+  blockNumber: 0,
+  previousHash: 'Genesis'
+}
 
 export {Block}
