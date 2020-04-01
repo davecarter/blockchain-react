bc78fe1ceffbddb3f430501e6ad473cf70f9ba58
David Garcia <david.garcia@adevinta.com>
2020-04-01 21:59:13 +0200

    refactor(components): refactor previous block component implementation

--- src/domain/block/requests/factory.js
+++
@@ -1,6 +0,0 @@
-import {BlockRequest} from './blockRequest'
-
-export class BlockRequestsFactory {
-  static blockRequest = ({blockId, previousHash}) =>
-    new BlockRequest({blockId, previousHash})
-}
