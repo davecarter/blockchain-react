9197a4cf616c4dec3f1011ea958ef76b76f35d07
David Garcia <david.garcia@adevinta.com>
2020-04-05 09:16:50 +0200

    feat(components): adds a new blockChainList component

---
+++ src/components/blockChainList/index.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-unused-vars */
+import React, {useState, useEffect} from 'react'
+import {domain} from '../../domain/index'
+import {config} from '../../domain/config'
+import {Block} from '../block'
+
+const BlockChainList = () => {
+  const [blockChainData, setBlockChainData] = useState([])
+  const [genesisBlock, setGenesisBlock] = useState('')
+
+  const genesisBlockData = config.GENESIS_BLOCK
+
+  useEffect(() => {
+    domain
+      .get('get_blockchain_use_case')
+      .execute()
+      .then(data => {
+        setBlockChainData(data)
+        if (data.length === 0) {
+          domain
+            .get('set_genesis_block_use_case')
+            .execute({blockData: genesisBlockData})
+            .then(data => setGenesisBlock(data))
+        }
+      })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return blockChainData.map(block => {
+    const {
+      blockId,
+      previousHash,
+      blockData,
+      creationDate,
+      currentDifficulty,
+      hash
+    } = block.blockData
+
+    return (
+      <div key={blockId}>
+        <Block
+          blockId={blockId}
+          previousHash={previousHash}
+          blockData={blockData}
+          creationDate={creationDate}
+          currentDifficulty={currentDifficulty}
+          hash={hash}
+        />
+      </div>
+    )
+  })
+}
+
+export {BlockChainList}
