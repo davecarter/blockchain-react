524e69bc8d501e6f3fb796da283d48469aeedd9c
David Garcia <david.garcia@adevinta.com>
2020-07-14 19:33:27 +0200

    fix typo in test description

--- src/__test__/GetCurrentBtcPriceUseCase.test.js
+++ src/__test__/GetCurrentBtcPriceUseCase.test.js
@@ -3,7 +3,7 @@ import {domain} from '../domain'
 
 const expect = global.expect
 
-test('Given a contry code should return the current Bitcoin price converted to Euro', async () => {
+test('Given a country code should return the current Bitcoin price converted to Euro', async () => {
   const useCaseResponse = await domain
     .get('get_btc_current_price_use_case')
     .execute({fiatCurrencyCode: 'EUR'})
