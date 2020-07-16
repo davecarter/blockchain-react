4ff8ee19eb530534e4b3cdfb13209b6bbcff03e1
David Garcia <david.garcia@adevinta.com>
2020-07-16 22:32:48 +0200

    refactor ledger component

--- src/components/ledger/index.js
+++ src/components/ledger/index.js
@@ -1,35 +1,61 @@
-import React, {useEffect} from 'react'
-
-import {config} from '../../domain/config'
+import React, {useEffect, useState} from 'react'
 import {domain} from '../../domain/index'
 
-// import {BlockChainList} from '../blockChainList'
+import {BlockChainList} from '../blockChainList'
 import {Block} from '../block'
 
 const Ledger = () => {
-  const {id, difficulty, hash, nonce, previousHash} = config.GENESIS_BLOCK
+  const [blockChainData, setBlockChainData] = useState([])
+  const [lastMinedBlock, setLastMinedBlock] = useState({
+    lastId: 0,
+    lastPreviousHash: '',
+    lastHash: '',
+    lastDifficulty: '',
+    lastNonce: 0
+  })
 
   useEffect(() => {
     domain
       .get('get_blockchain_use_case')
       .execute()
       .then(data => {
-        return data
+        setBlockChainData(data)
       })
   }, [])
 
+  useEffect(() => {
+    if (blockChainData.length > 0) {
+      const {id, previousHash, difficulty, nonce} = blockChainData[
+        blockChainData.length - 1
+      ]
+      setLastMinedBlock({
+        lastId: id,
+        lastPreviousHash: previousHash,
+        lastDifficulty: difficulty,
+        lastNonce: nonce
+      })
+    }
+  }, [blockChainData])
+
+  const {
+    lastId,
+    lastPreviousHash,
+    lastHash,
+    lastDifficulty,
+    lastNonce
+  } = lastMinedBlock
   return (
     <div>
       <h3 style={{textAlign: 'center'}}>Ledger Blockchain list</h3>
       <Block
-        id={id}
-        previousHash={previousHash}
-        hash={hash}
-        difficulty={difficulty}
-        nonce={nonce}
+        id={lastId}
+        previousHash={lastPreviousHash}
+        hash={lastHash}
+        difficulty={lastDifficulty}
+        nonce={lastNonce}
         isMined={false}
       />
-      {/* <BlockChainList blockChainList={blockChainList} /> */}
+      <BlockChainList blockChainList={blockChainData} />
     </div>
   )
 }
