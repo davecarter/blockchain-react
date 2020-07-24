1240b75fd507efdb74340890d263d1fbda25a735
David Garcia <david.garcia@adevinta.com>
2020-07-24 12:57:58 +0200

    gets mining logic into a mapper

--- src/domain/blockchain/mapper/fromBlockDataVOToMinedRawBlockMapper.js
+++ src/domain/blockchain/mapper/fromBlockDataVOToMinedRawBlockMapper.js
@@ -4,8 +4,27 @@ class FromBlockDataVOToMinedRawBlockMapper {
     this._SHA256 = SHA256
   }
 
+  _createHash = ({id, userData, nonce}) => {
+    const hash = this._SHA256(id + userData + nonce).toString()
+    return hash
+  }
+
+  _handleMiner = ({id, userData, nonce, hash}) => {
+    const difficulty = this._config.GENESIS_BLOCK.difficulty
+    while (!hash.startsWith(difficulty)) {
+      nonce++
+      hash = this._createHash({id, userData, nonce})
+    }
+
+    return hash
+  }
+
   map({blockDataVO}) {
-    return blockDataVO
+    const id = blockDataVO.id()
+    const userData = blockDataVO.userData()
+    const nonce = blockDataVO.nonce()
+    const hash = blockDataVO.hash()
+    return this._handleMiner({id, userData, nonce, hash})
   }
 }
 
