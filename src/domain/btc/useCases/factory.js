0ae66e332962829503c52e8033d66e780ee6a733
David G <dave74@gmail.com>
2020-11-05 08:08:21 +0100

    add minor fixes

--- src/domain/btc/useCases/factory.js
+++ src/domain/btc/useCases/factory.js
@@ -1,4 +1,4 @@
-import {GetCurrentBtcPriceUseCase} from './GetCurrentBtcPriceUseCase'
+import {GetCurrentBtcPriceUseCase} from './getCurrentBtcPriceUseCase'
 import {BtcRepositoryFactory} from '../repositories/factory'
 import {CurrenciesValueObjectFactory} from '../valueObjects/factory'
 
